43ac7a984077856578c958238655c4aede516ae2
Vick <23300018@uttt.edu.mx>
2025-06-25 19:39:58 -0600

    Cambio setiingssssss

--- gestionpaqueteria/settings.py
+++ gestionpaqueteria/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 
-import pymysql
-pymysql.install_as_MySQLdb()
+# import pymysql
+# pymysql.install_as_MySQLdb()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-em8=&gg99_!=6bfn%-t(v$lblk62ux)mewfu8m#gt(m3$ubo#x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['paqueteria.herokuapp.com', '127.0.0.1', 'localhost']
 
